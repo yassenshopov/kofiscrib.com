5d7bae7ef20cf2326230aa93aaa57d3253bf2bab
Yassen Shopov <yassenshopov00@gmail.com>
2022-03-04 01:24:45 +0000

    Added submenus to main pages

--- common/menuJS.js
+++ common/menuJS.js
@@ -10,3 +10,27 @@ function animate_menu(x) {
     }
     
 }
+
+function onHoverMenu1() {
+    document.getElementById("dropMenu1").style.display = "flex";
+}
+
+function onHoverAwayMenu1() {
+    document.getElementById("dropMenu1").style.display = "none";
+}
+
+function onHoverMenu2() {
+    document.getElementById("dropMenu2").style.display = "flex";
+}
+
+function onHoverAwayMenu2() {
+    document.getElementById("dropMenu2").style.display = "none";
+}
+
+function onHoverMenu3() {
+    document.getElementById("dropMenu3").style.display = "flex";
+}
+
+function onHoverAwayMenu3() {
+    document.getElementById("dropMenu3").style.display = "none";
+}
